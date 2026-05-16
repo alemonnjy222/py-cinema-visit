10fd8c939d6e2874029b1eb38631bf0309f007cd
alemonnjy222 <71006485+alemonnjy222@users.noreply.github.com>
2026-05-16 07:52:11 +0200

    Update hall.py

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -2,11 +2,12 @@ class CinemaHall:
     def __init__(self, number: int) -> None:
         self.hall_number = number
 
-    def movie_session(self,
-                      movie_name: str,
-                      customers: str,
-                      cleaning_staff: str
-                     ) -> None:
+    def movie_session(
+        self,
+        movie_name: str,
+        customers: list,
+        cleaning_staff: str
+    ) -> None:
         print(f'"{movie_name}" started in hall number {self.hall_number}.')
         for customer in customers:
             customer.watch_movie(movie_name)
